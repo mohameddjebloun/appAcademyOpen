04532974d2b19f645de02d2dbdb2df39ab69d65e
mohameddjebloun <djeblounm@gmail.com>
2024-02-28 01:39:49 +0100

    Solve the muscle memory challenge

---
+++ JavaScript Fundamentals/Muscle Memory/index.js
@@ -0,0 +1,74 @@
+/*Practice typing the syntax for functions. Write 10 functions with different messages and console log them. Focus on using proper
+ style and indentation. Be sure to test your code by running it after each repetition.*/
+
+// Function to say hello with a custom name
+function sayHello(name) {
+  let msg = "Hello, " + name + ". How are you?";
+  return msg;
+}
+
+// Function to say hi with a custom name
+function sayHi(name) {
+  let msg = "Hi, " + name + ". How are you?";
+  return msg;
+}
+
+// Function to introduce oneself with a custom name
+function introduceYourself(name) {
+  let msg = "Hey, my name is " + name + "!";
+  return msg;
+}
+
+// Function to apologize
+function apologize() {
+  let msg = "I'm sorry";
+  return msg;
+}
+
+// Function to reveal age
+function revealAge(age) {
+  let msg = "I'm " + age + " years old.";
+  return msg;
+}
+
+// Function to say goodbye with a custom name
+function sayGoodbye(name) {
+  let msg = "Bye " + name + ".";
+  return msg;
+}
+
+// Function to say thank you
+function sayThankYou() {
+  let msg = "Thank you!";
+  return msg;
+}
+
+// Function to say welcome
+function sayWelcome() {
+  let msg = "Welcome!";
+  return msg;
+}
+
+// Function to wish happy birthday with a custom name
+function sayHappyBirthday(name) {
+  let msg = "Happy birthday " + name + "!";
+  return msg;
+}
+
+// Function to respond with "You're welcome!"
+function sayYouAreWelcome() {
+  let msg = "You're welcome!";
+  return msg;
+}
+
+// Testing the functions
+console.log(sayHello("world")); // => Hello, world. How are you?
+console.log(sayHi("world")); // => Hi, world. How are you?
+console.log(introduceYourself("Mohamed")); // => Hey, my name is Mohamed!
+console.log(apologize()); // => I'm sorry
+console.log(revealAge(21)); // => I'm 21 years old.
+console.log(sayGoodbye("brother")); // => Bye brother.
+console.log(sayThankYou()); // => Thank you!
+console.log(sayWelcome()); // => Welcome!
+console.log(sayHappyBirthday("Batata")); // => Happy birthday Batata!
+console.log(sayYouAreWelcome()); // => You're welcome!
