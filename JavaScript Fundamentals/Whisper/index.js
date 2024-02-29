174f5e62a1b3fb697f862ea9b039178a4a66ee39
mohameddjebloun <djeblounm@gmail.com>
2024-02-29 09:38:31 +0100

    Solve the Whisper challenge

---
+++ JavaScript Fundamentals/Whisper/index.js
@@ -0,0 +1,7 @@
+// Write a function whisper that takes in a string and returns a "whispered" version of that string.
+function whisper(str) {
+  return "..." + str.toLowerCase() + "...";
+}
+
+console.log(whisper("Hey Buddy")); // => "...hey buddy..."
+console.log(whisper("YEA! that was fun")); // => "...yea! that was fun..."
