41be9044b577d990b9cee38f65fd0298861daf08
mohameddjebloun <djeblounm@gmail.com>
2024-03-01 01:24:14 +0100

    Solve the Print Fives challenge

---
+++ JavaScript Fundamentals/Control Flow/Loops/Print Fives/index.js
@@ -0,0 +1,22 @@
+/*Write a function printFives(max) that prints out the multiples of 5 that are less than the max.
+
+Try to solve this in two ways, one using an if statement, and one without.*/
+function printFives(max) {
+  /*let i = 0;
+  while (i < max) {
+    console.log(i);
+    i += 5;
+  }*/
+  for (let i = 0; i < max; i++) {
+    if (i % 5 === 0) {
+      console.log(i);
+    }
+  }
+}
+// Example:
+
+printFives(20); // prints out:
+// 0
+// 5
+// 10
+// 15
