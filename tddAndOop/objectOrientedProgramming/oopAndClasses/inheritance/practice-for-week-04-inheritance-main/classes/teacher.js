e5f18cf93710623677b6b7fcbf72f15434b2452f
djeblounm <djeblounm@gmail.com>
2025-02-11 14:59:57 +0000

    Finish the inheritance practice

--- tddAndOop/objectOrientedProgramming/oopAndClasses/inheritance/practice-for-week-04-inheritance-main/classes/teacher.js
+++ tddAndOop/objectOrientedProgramming/oopAndClasses/inheritance/practice-for-week-04-inheritance-main/classes/teacher.js
@@ -1,7 +1,20 @@
-const Person = require('./person');
+const Person = require("./person");
 
 // Your code here
-
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+  static combinedYearsOfExperience(teachers) {
+    let years = 0;
+    teachers.forEach((teacher) => {
+      years += teacher.yearsOfExperience;
+    });
+    return years;
+  }
+}
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
