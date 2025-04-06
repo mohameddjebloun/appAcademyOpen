d5ef66ba72df5c5dd20b99e3378396a0ba686fde
mohameddjebloun <djeblounm@gmail.com>
2025-04-07 00:39:53 +0100

    Finish phase 4b of the Manager and Employee Salaries project

--- tddAndOop/basicProjects/longPracticeManagerAndEmployeeSalaries/manager.js
+++ tddAndOop/basicProjects/longPracticeManagerAndEmployeeSalaries/manager.js
@@ -7,14 +7,30 @@ class Manager extends Employee {
   addEmployee(employee) {
     this.employees.push(employee);
   }
+  _totalSubSalary() {
+    let sum = 0;
+    if (this.employees.length === 0) {
+      return sum;
+    }
+    for (let i = 0; i < this.employees.length; i++) {
+      if (this.employees[i] instanceof Manager) {
+        sum += this.employees[i]._totalSubSalary();
+      }
+      sum += this.employees[i].salary;
+    }
+    return sum;
+  }
+  calculateBonus(multiplier) {
+    return (this.salary + this._totalSubSalary()) * multiplier;
+  }
 }
 const splinter = new Manager("Splinter", 100000, "Sensei");
-console.log("Before: ", splinter);
+const leo = new Manager("Leonardo", 90000, "Ninja", splinter);
+const raph = new Manager("Raphael", 90000, "Ninja", leo);
+const mikey = new Employee("Michelangelo", 85000, "Grasshopper", raph);
+const donnie = new Employee("Donatello", 85000, "Grasshopper", raph);
 
-const leo = new Employee("Leonardo", 90000, "Ninja", splinter);
-const mikey = new Employee("Michelangelo", 90000, "Ninja", splinter);
-const donnie = new Employee("Donatello", 90000, "Ninja", splinter);
-const raph = new Employee("Raphael", 90000, "Ninja", splinter);
-
-console.log("After: ", splinter);
+console.log(splinter.calculateBonus(0.05)); // => 22500
+console.log(leo.calculateBonus(0.05)); // => 17500
+console.log(raph.calculateBonus(0.05)); // => 13000
 module.exports = Manager;
