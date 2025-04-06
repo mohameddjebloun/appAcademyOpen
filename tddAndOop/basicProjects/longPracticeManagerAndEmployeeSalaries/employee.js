a60ca5e5b91440223ad9cbeccf304aa6c401b0a1
mohameddjebloun <djeblounm@gmail.com>
2025-04-07 00:34:19 +0100

    Finish phase 4a of the Manager and Employee Salaries project

--- tddAndOop/basicProjects/longPracticeManagerAndEmployeeSalaries/employee.js
+++ tddAndOop/basicProjects/longPracticeManagerAndEmployeeSalaries/employee.js
@@ -8,8 +8,14 @@ class Employee {
       manager.addEmployee(this);
     }
   }
+  calculateBonus(multiplier) {
+    return this.salary * multiplier;
+  }
 }
-const leo = new Employee("Leonardo", 90000, "Ninja");
-console.log(leo);
+const raph = new Employee("Raphael", 90000, "Ninja");
+const donny = new Employee("Donatello", 85000, "Grasshopper");
+
+console.log(raph.calculateBonus(0.25)); // => 22500
+console.log(donny.calculateBonus(0.15)); // => 12750
 
 module.exports = Employee;
