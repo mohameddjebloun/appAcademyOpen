c3eeac574dd5520541eddde078b73441cdcb5d77
mohameddjebloun <djeblounm@gmail.com>
2025-04-07 20:04:52 +0100

    Finish phase 3 of the Friendly and Evil Dragons project

--- tddAndOop/basicProjects/friendlyAndEvilDragons/classes/evil-dragon.js
+++ tddAndOop/basicProjects/friendlyAndEvilDragons/classes/evil-dragon.js
@@ -1,1 +1,41 @@
 // Your code here
+const Dragon = require( "./dragon" );
+class EvilDragon extends Dragon
+{
+    constructor ( name, color, evilDoings, nemesis )
+    {
+        super( name, color );
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner ()
+    {
+        for ( let i = 0; i < this.evilDoings.length; i++ )
+        {
+            console.log( `${ this.name } will ${ this.evilDoings[ i ] }` );
+        }
+    }
+
+    burnsNemesis ()
+    {
+        return `${ this.name } destroys ${ this.nemesis } with fire! WHOOOSH!!!`;
+    }
+}
+
+const drogon = new EvilDragon(
+    "Drogon",
+    "black-red",
+    [
+        "eat all your livestock",
+        "burn down your castle in Westeros",
+        "burn your father and brother"
+    ],
+    "Night King"
+);
+console.log( drogon );
+drogon.dontInviteThemOverForDinner();
+console.log( drogon.breathesFire() );
+console.log( drogon.burnsNemesis() );
+
+module.exports = EvilDragon;
