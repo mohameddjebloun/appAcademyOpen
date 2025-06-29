31c3358fd9d04b7635d77473d1d1f01b9ced460c
mohameddjebloun <djeblounm@gmail.com>
2025-06-29 03:38:44 +0100

    Implement instance methods

--- tddAndOop/challengeProjects/adventureProject/class/player.js
+++ tddAndOop/challengeProjects/adventureProject/class/player.js
@@ -1,56 +1,63 @@
 class Player {
+  constructor(name, startingRoom) {
+    this.name = name;
+    this.currentRoom = startingRoom;
+    this.items = [];
+  }
 
-    constructor(name, startingRoom) {
-        this.name = name;
-        this.currentRoom = startingRoom;
-        this.items = [];
+  move(direction) {
+    const nextRoom = this.currentRoom.getRoomInDirection(direction);
+
+    // If the next room is valid, set the player to be in that room
+    if (nextRoom) {
+      this.currentRoom = nextRoom;
+
+      nextRoom.printRoom(this);
+    } else {
+      console.log("You cannot move in that direction");
     }
+  }
 
-    move(direction) {
-
-        const nextRoom = this.currentRoom.getRoomInDirection(direction);
-
-        // If the next room is valid, set the player to be in that room
-        if (nextRoom) {
-            this.currentRoom = nextRoom;
-
-            nextRoom.printRoom(this);
-        } else {
-            console.log("You cannot move in that direction");
-        }
+  printInventory() {
+    if (this.items.length === 0) {
+      console.log(`${this.name} is not carrying anything.`);
+    } else {
+      console.log(`${this.name} is carrying:`);
+      for (let i = 0; i < this.items.length; i++) {
+        console.log(`  ${this.items[i].name}`);
+      }
     }
+  }
 
-    printInventory() {
-        if (this.items.length === 0) {
-            console.log(`${this.name} is not carrying anything.`);
-        } else {
-            console.log(`${this.name} is carrying:`);
-            for (let i = 0 ; i < this.items.length ; i++) {
-                console.log(`  ${this.items[i].name}`);
-            }
-        }
+  takeItem(itemName) {
+    const item = this.currentRoom.getItemByName(itemName);
+    if (item) {
+      this.currentRoom.items = this.currentRoom.items.filter((i) => i !== item); // remove from room
+      this.items.push(item);
     }
+  }
 
-    takeItem(itemName) {
-
-        // Fill this in
-
+  dropItem(itemName) {
+    const item = this.getItemByName(itemName);
+    if (item) {
+      this.items = this.items.filter((i) => i !== item);
+      this.currentRoom.items.push(item);
     }
+  }
 
-    dropItem(itemName) {
-
-        // Fill this in
+  eatItem(itemName) {
+    const item = this.getItemByName(itemName);
+    if (item && item instanceof require("./food").Food) {
+      // Remove the item from inventory
+      this.items = this.items.filter((i) => i !== item);
+    } else {
+      console.log(`${itemName} is not edible.`);
     }
+  }
 
-    eatItem(itemName) {
-        // Fill this in
-
-    }
-
-    getItemByName(name) {
-
-        // Fill this in
-    }
+  getItemByName(name) {
+    return this.items.find((item) => item.name === name);
+  }
 }
 
 module.exports = {
